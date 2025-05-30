bdd39fb73dda7588f7dd4a622f83eb1ea9217b56
AnnasophiaSanchez-Acosta <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-05-30 13:30:39 -0400

    added js files

---
+++ js/gameScene.js
@@ -0,0 +1,8 @@
+// Created by: Anna S.
+// Created on: May 2025
+// This is Game Scene
+
+/** 
+ * This class is Game Scene
+ */
+
--- js/menuScene.js
+++ js/menuScene.js
@@ -1,1 +1,11 @@
-// 
+// Created by: Anna S.
+// Created on: May 2025
+// This is Game Scene
+
+
+/** 
+ * This class is Menu Scene
+ */
+ preload() {
+    console.log("Menu Scene")
+  }
