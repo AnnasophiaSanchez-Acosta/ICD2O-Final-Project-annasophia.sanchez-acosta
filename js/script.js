7ba1133d36b6706d5d1b173e6a1710a056fa135c
AnnasophiaSanchez-Acosta <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-06-03 20:54:39 +0000

    added files to restart

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Created by: Anna S.
+// Created: Jun 2025
+
+'use strict'
+
+/**
+ * This function flips coin 
+ */
+
+function flipButton () {
+  const randomNumber = Math.floor(Math.random()) 
+  document.getElementById('Heads').innerHTML
+  document.getElementById('Tails').innerHTML
+}
