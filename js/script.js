e83120505b0ff563ed202a1970767c515e576e0e
AnnasophiaSanchez-Acosta <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-06-03 21:41:09 +0000

    button and spaces?

--- js/script.js
+++ js/script.js
@@ -1,14 +1,2 @@
 // Created by: Anna S.
 // Created: Jun 2025
-
-'use strict'
-
-/**
- * This function flips coin 
- */
-
-function flipButton () {
-  const randomNumber = Math.floor(Math.random()) 
-  document.getElementById('Heads').innerHTML
-  document.getElementById('Tails').innerHTML
-}
