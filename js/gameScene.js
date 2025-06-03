5eb6c495345c533a4b23f0f8ae7326e32a3a848e
AnnasophiaSanchez-Acosta <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-06-03 00:41:40 +0000

    Spaces in gameScene

--- js/gameScene.js
+++ js/gameScene.js
@@ -4,7 +4,7 @@
 // This is Game Scene
 
 class GameScene extends Phaser.Scene {
-  constructor() {
+  constructor () {
     super({ key: 'gameScene' })
 
     this.coinImage = null
@@ -14,27 +14,27 @@ class GameScene extends Phaser.Scene {
     this.flipCount = 0
   }
 
-  preload() {
+  preload () {
     this.load.image('heads', './images/heads-coin.png')
     this.load.image('tails', './images/tails-coin.png')
     this.load.image('flipButton', './images/flipButton.png')
   }
 
-  create() {
+  create () {
     this.coinImage = this.add
       .image(this.cameras.main.centerX, 300, 'heads')
       .setScale(0.5)
 
     this.scoreText = this.add.text(20, 20, 'Flips: 0', {
       fontSize: '32px',
-      color: "#000000",
-    });
+      color: '#000000'
+    })
 
     this.resultText = this.add.text(20, 60, '', {
       // new text below flips count
       fontSize: '28px',
-      color: '#000000',
-    });
+      color: '#000000'
+    })
 
     this.flipButton = this.add
       .image(this.cameras.main.centerX, 650, 'flipButton')
@@ -43,16 +43,16 @@ class GameScene extends Phaser.Scene {
     this.flipButton.on('pointerdown', () => this.flipCoin())
   }
 
-  flipCoin() {
+  flipCoin () {
     let answer
     if (Math.random() < 0.5) {
       answer = 'heads'
     } else {
       answer = 'tails'
     }
-  
+
     this.coinImage.setTexture(answer) // ← update the coin image
-  
+
     this.flipCount++
     this.scoreText.setText('Flips: ' + this.flipCount)
     this.resultText.setText(
